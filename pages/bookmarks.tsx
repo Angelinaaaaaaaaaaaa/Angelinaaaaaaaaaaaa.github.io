757d4d8c525c8d8d566171970ce70f9bad47e46b
COURSER XU <courser_xu@hotmail.com>
2024-11-21 11:42:47 +0800

    Update

--- pages/bookmarks.tsx
+++ pages/bookmarks.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import { GetStaticProps } from 'next';
 import { Container, Grid, Text, Title } from '@components';
-
-import { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints';
-import { getBookmarks } from '../notion';
 
 const Bookmarks = (): JSX.Element => (
   <Container marginBottom="5rem">
@@ -33,43 +29,5 @@ export interface Bookmark {
   name: string;
   url: string;
 }
-
-const formatBookmarks = ({
-                           results,
-                         }: QueryDatabaseResponse): ReadonlyArray<Bookmark> =>
-  results.reduce<Array<Bookmark>>((acc, result) => {
-    if (
-      result.object === 'page' &&
-      'url' in result &&
-      result.properties?.Created?.type === 'created_time' &&
-      result.properties?.URL?.type == 'url' &&
-      result.properties.URL.url &&
-      result.properties?.Name?.type == 'title' &&
-      result.properties.Name.title?.[0]?.type === 'text'
-    ) {
-      return [
-        ...acc,
-        {
-          id: result.id,
-          url: result.properties.URL.url,
-          created: result.properties.Created.created_time,
-          name: result.properties.Name.title[0].plain_text,
-        },
-      ];
-    }
-
-    return acc;
-  }, []);
-
-export const getStaticProps: GetStaticProps = async () => {
-  const bookmarks = await getBookmarks();
-
-  return {
-    props: {
-      bookmarks: formatBookmarks(bookmarks),
-    },
-    revalidate: 60, // Optional: Revalidate the page every 60 seconds
-  };
-};
 
 export default Bookmarks;
