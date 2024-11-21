b9414518313ec2f6f0526381d6a9c3be5b2eee25
COURSER XU <courser_xu@hotmail.com>
2024-11-21 09:44:44 +0800

    Update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://Angelinaaaaaaaaaaaa.github.io',
+  siteUrl: process.env.SITE_URL || 'https://Angelinaaaaaaaaaaaa.github.io/',
   generateRobotsTxt: true,
 };
